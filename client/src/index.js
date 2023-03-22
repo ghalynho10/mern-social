63690a095723069efe240dbe125a2be5c20ec755
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-03-22 18:17:27 -0400

    finished Color, theme, darkmode and styling

--- client/src/index.js
+++ client/src/index.js
@@ -18,14 +18,14 @@ import {
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = { key: root, storage, version: 1 };
+const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
-    getDefaultMiddleware({
+    return getDefaultMiddleware({
       serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     });
   },
